fc10c21edb5b99b0c35bcda34a0e2ba5cda7e24c
QmppmQ <878083064@qq.com>
2021-03-12 20:57:56 +0800

    update verilator-model

---
+++ verilator-model/_test.s
@@ -0,0 +1,34 @@
+	.file	"test.c"
+	.option nopic
+	.attribute arch, "rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"%d"
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sd	ra,24(sp)
+	sd	s0,16(sp)
+	addi	s0,sp,32
+	li	a5,1
+	sw	a5,-20(s0)
+	lw	a5,-20(s0)
+	mv	a1,a5
+	lui	a5,%hi(.LC0)
+	addi	a0,a5,%lo(.LC0)
+	call	printf
+	li	a5,0
+	mv	a0,a5
+	ld	ra,24(sp)
+	ld	s0,16(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (GNU) 9.2.0"
